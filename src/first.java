9972cff732e45cdece89f01c4abe9c7de7e1bf11
sedafdemir <57167050+sedafdemir@users.noreply.github.com>
2019-11-10 11:40:38 -0600

    Update first.java

--- src/first.java
+++ src/first.java
@@ -1,5 +1,8 @@
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Made changes");
+        System.out.println("second cchange");
+        
     }
 }
